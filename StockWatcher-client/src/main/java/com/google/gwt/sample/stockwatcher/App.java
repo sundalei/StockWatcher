d3b4cc4e2fa8ee5541e68ec76615d02950815588
DaLei Sun <sundalei1988@gmail.com>
2020-06-17 15:53:56 +0800

    test

--- StockWatcher-client/src/main/java/com/google/gwt/sample/stockwatcher/App.java
+++ StockWatcher-client/src/main/java/com/google/gwt/sample/stockwatcher/App.java
@@ -1,6 +1,12 @@
 package com.google.gwt.sample.stockwatcher;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -53,5 +59,51 @@ public class App implements EntryPoint {
 
 		// Move cursor focus to the input box.
 		newSymbolTextBox.setFocus(true);
+
+		// Listen for mouse events on the Add button.
+		addStockButton.addClickHandler(new ClickHandler() {
+
+			/** {@inheritDoc} */
+			@Override
+			public void onClick(final ClickEvent event) {
+				addStock();
+			}
+		});
+
+		// Listen for keyboard events in the input box.
+		newSymbolTextBox.addKeyDownHandler(new KeyDownHandler() {
+
+			@Override
+			public void onKeyDown(final KeyDownEvent event) {
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					addStock();
+				}
+			}
+
+		});
+
+	}
+
+	/**
+	 * Add stock to FlexTable. Executed when the user clicks the addStockButton or presses enter in the
+	 * newSymbolTextBox.
+	 */
+	private void addStock() {
+		final String symbol = newSymbolTextBox.getText().toUpperCase();
+		newSymbolTextBox.setFocus(true);
+
+		// Stock code must be between 1 and 10 chars that are numbers, letters, or dots.
+		if (!symbol.matches("^[0-9A-Z\\.]{1,10}$")) {
+			Window.alert("'" + symbol + "' is not a valid symbol.");
+			newSymbolTextBox.selectAll();
+			return;
+		}
+
+		newSymbolTextBox.setText("");
+
+		// TODO Don't add the stock if it's already in the table.
+		// TODO Add the stock to the table
+		// TODO Add a button to remove this stock from the table.
+		// TODO Get the stock price.
 	}
 }
--- readme.txt
+++ readme.txt
@@ -1,2 +1,6 @@
 mvn gwt:codeserver -pl *-client -am
 mvn jetty:run -pl *-server -am -Denv=dev
+
+netstat -ano| findstr "9876"
+tasklist|findstr "24516"
+taskkill /f /t /im java.exe
