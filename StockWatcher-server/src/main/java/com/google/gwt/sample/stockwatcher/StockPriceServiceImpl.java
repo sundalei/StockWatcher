27f8bac7522b9534f7a324b9087afe9a061df439
DaLei Sun <sundalei1988@gmail.com>
2020-06-28 22:19:41 +0800

    test

---
+++ StockWatcher-server/src/main/java/com/google/gwt/sample/stockwatcher/StockPriceServiceImpl.java
@@ -0,0 +1,32 @@
+package com.google.gwt.sample.stockwatcher;
+
+import java.util.Random;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class StockPriceServiceImpl extends RemoteServiceServlet implements StockPriceService {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    private static final double MAX_PRICE = 100.0;
+    private static final double MAX_PRICE_CHANGE = 0.02;
+
+    @Override
+    public StockPrice[] getPrices(String[] symbols) {
+        Random rnd = new Random();
+
+        StockPrice[] prices = new StockPrice[symbols.length];
+        for (int i=0; i<symbols.length; i++) {
+            double price = rnd.nextDouble() * MAX_PRICE;
+            double change = price * MAX_PRICE_CHANGE * (rnd.nextDouble() * 2f - 1f);
+        
+            prices[i] = new StockPrice(symbols[i], price, change);
+        }
+        
+        return prices;
+    }
+    
+}
